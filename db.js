8f606d4a50ec2d13542de86afdd73c45d4988f92
OmegaJastip <jastipomega@gmail.com>
2025-10-01 19:58:05 +0700

    up

---
+++ db.js
@@ -0,0 +1,64 @@
+let db;
+
+const openDB = () => {
+  return new Promise((resolve, reject) => {
+    const request = indexedDB.open('PosMotorDB', 1);
+    request.onerror = () => reject(request.error);
+    request.onsuccess = () => {
+      db = request.result;
+      resolve(db);
+    };
+    request.onupgradeneeded = (event) => {
+      const db = event.target.result;
+      if (!db.objectStoreNames.contains('inventory')) {
+        db.createObjectStore('inventory', { keyPath: 'id', autoIncrement: true });
+      }
+      if (!db.objectStoreNames.contains('sales')) {
+        db.createObjectStore('sales', { keyPath: 'id', autoIncrement: true });
+      }
+      if (!db.objectStoreNames.contains('customers')) {
+        db.createObjectStore('customers', { keyPath: 'id', autoIncrement: true });
+      }
+    };
+  });
+};
+
+const addItem = (storeName, item) => {
+  return new Promise((resolve, reject) => {
+    const transaction = db.transaction([storeName], 'readwrite');
+    const store = transaction.objectStore(storeName);
+    const request = store.add(item);
+    request.onsuccess = () => resolve(request.result);
+    request.onerror = () => reject(request.error);
+  });
+};
+
+const getAllItems = (storeName) => {
+  return new Promise((resolve, reject) => {
+    const transaction = db.transaction([storeName], 'readonly');
+    const store = transaction.objectStore(storeName);
+    const request = store.getAll();
+    request.onsuccess = () => resolve(request.result);
+    request.onerror = () => reject(request.error);
+  });
+};
+
+const updateItem = (storeName, item) => {
+  return new Promise((resolve, reject) => {
+    const transaction = db.transaction([storeName], 'readwrite');
+    const store = transaction.objectStore(storeName);
+    const request = store.put(item);
+    request.onsuccess = () => resolve(request.result);
+    request.onerror = () => reject(request.error);
+  });
+};
+
+const deleteItem = (storeName, id) => {
+  return new Promise((resolve, reject) => {
+    const transaction = db.transaction([storeName], 'readwrite');
+    const store = transaction.objectStore(storeName);
+    const request = store.delete(id);
+    request.onsuccess = () => resolve(request.result);
+    request.onerror = () => reject(request.error);
+  });
+};
